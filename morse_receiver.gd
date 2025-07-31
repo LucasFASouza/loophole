71336c504bc015f28cc6483adfa884e53664f502
Lucas Felipe <outrolucas2002@gmail.com>
2025-07-31 00:07:25 -0300

    use timer

--- morse_receiver.gd
+++ morse_receiver.gd
@@ -1,36 +1,40 @@
 extends Node
 
 const TIME_DASH := 0.5
-const TIME_WORD := 1.0
+
+# Morse code dictionary loaded from JSON
+var morse_dict: Array = []
 
 @onready var writting: Label = %Writting
 @onready var meaning: Label = %Meaning
-@onready var word_timer: Timer = $WordTimer
-var _pressed_time: int = 0
+@onready var dash_timer: Timer = $DashTimer
 
-var _is_holding := false
-var _last_dot_index := -1
-
+func _ready() -> void:
+	dash_timer.timeout.connect(_on_dash_timeout)
+	var file = FileAccess.open("res://morse.json", FileAccess.READ)
+	if file:
+		var json_text = file.get_as_text()
+		var result = JSON.parse_string(json_text)
+		if typeof(result) == TYPE_ARRAY:
+			morse_dict = result
+		file.close()
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		_pressed_time = Time.get_ticks_msec()
 		writting.text += "."
-		_is_holding = true
-		_last_dot_index = writting.text.length() - 1
-		if word_timer.is_stopped() == false:
-			word_timer.stop()
-
-	if _is_holding:
-		var duration = (Time.get_ticks_msec() - _pressed_time) / 1000.0
-		if duration > TIME_DASH and _last_dot_index >= 0 and writting.text[_last_dot_index] == ".":
-			writting.text = writting.text.substr(0, _last_dot_index) + "-" + writting.text.substr(_last_dot_index + 1)
-
+		dash_timer.start(TIME_DASH)
+	
 	if Input.is_action_just_released("ui_accept"):
-		_is_holding = false
-		_last_dot_index = -1
-		word_timer.start(TIME_WORD)
+		dash_timer.stop()
+		var found = false
+		for entry in morse_dict:
+			if entry.has("input") and entry["input"] == writting.text:
+				meaning.text = entry["meaning"]
+				found = true
+				break
+		if not found:
+			meaning.text = ""
 
-# Called when word_timer times out
-func _on_word_timer_timeout():
-	writting.text += "/"
+func _on_dash_timeout() -> void:
+	if writting.text.length() > 0:
+		writting.text = writting.text.substr(0, writting.text.length() - 1) + "-"
